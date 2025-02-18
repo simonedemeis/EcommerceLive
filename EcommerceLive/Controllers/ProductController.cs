33b5d36f231b43f280bcd90bc65ea66aa3a9ecf8
s.demeis <s.demeis@capoweb.it>
2025-02-18 13:00:08 +0100

    fix

--- EcommerceLive/Controllers/ProductController.cs
+++ EcommerceLive/Controllers/ProductController.cs
@@ -42,5 +42,32 @@ namespace EcommerceLive.Controllers
 
             return View(productsList);
         }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(AddProductModel addProductModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Add");
+            }
+
+            var newProduct = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = addProductModel.Name,
+                Description = addProductModel.Description,
+                Category = addProductModel.Category,
+                Price = addProductModel.Price,
+            };
+
+            products.Add(newProduct);
+
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ EcommerceLive/Models/AddProductModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceLive.Models
+{
+    public class AddProductModel
+    {
+        [Required(ErrorMessage = "Il nome è obbligatorio!")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "La descrizione è obbligatoria!")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "La categoria è obbligatoria!")]
+        public string? Category { get; set; }
+
+        [Required(ErrorMessage = "Il prezzo è obbligatorio!")]
+        public decimal Price { get; set; }
+    }
+}
